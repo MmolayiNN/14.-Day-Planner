5e35c28ca64b4dd3c15ad957a72d7f5eff16fbb7
Hohm <Hohm@HOHM>
2024-01-19 14:06:50 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Activities = New ListBox()
+        Label2 = New Label()
+        Label1 = New Label()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        SuspendLayout()
+        ' 
+        ' Activities
+        ' 
+        Activities.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Activities.FormattingEnabled = True
+        Activities.ItemHeight = 23
+        Activities.Location = New Point(321, 89)
+        Activities.Margin = New Padding(4, 3, 4, 3)
+        Activities.Name = "Activities"
+        Activities.Size = New Size(289, 211)
+        Activities.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(99, 89)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(78, 23)
+        Label2.TabIndex = 3
+        Label2.Text = "Weather"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(321, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(118, 23)
+        Label1.TabIndex = 4
+        Label1.Text = "Daily planner"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(94, 126)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(130, 27)
+        RadioButton1.TabIndex = 5
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "sunnyButton"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(94, 170)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(136, 27)
+        RadioButton2.TabIndex = 6
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "cloudyButton"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(94, 217)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(125, 27)
+        RadioButton3.TabIndex = 7
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "rainyButton"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 23F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.DeepSkyBlue
+        ClientSize = New Size(1000, 518)
+        Controls.Add(RadioButton3)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(Label1)
+        Controls.Add(Label2)
+        Controls.Add(Activities)
+        Font = New Font("Segoe UI", 10.2F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Margin = New Padding(4, 3, 4, 3)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Activities As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If RadioButton1.Checked = True Then
+
+
+            MsgBox("Hi since the weather is sunny today. Here are your daily activities:")
+
+            Activities.Items.Add("Go for a walk")
+            Activities.Items.Add("Have a picnic with friends")
+            Activities.Items.Add("Play outdoor sport like soccer or netball")
+
+            MsgBox("Hi, Since it is sunny outside, I suggest you go for a walk, have a picnic with friends or play an outdoor sport")
+
+        ElseIf RadioButton2.Checked = True Then
+            Activities.Items.Clear()
+
+            MsgBox("Hi,since the weather is cloudy today. Here are your daily activities:")
+
+            Activities.Items.Add("Read a book at a cozy cafe.")
+            Activities.Items.Add("Watch a movie at home or in a theater")
+            Activities.Items.Add("Visit a museum or art gallery.")
+
+            MsgBox("Hi, Since it is cloudy outside, I suggest you Read a book at a cozy cafe,Watch a movie at home or in a theater or Visit a museum or art gallery.")
+
+        ElseIf RadioButton3.Checked = True Then
+
+            MsgBox("Hi,since the weather is rainy today. Here are your daily activities:")
+
+            Activities.Items.Add("Stay indoors and watch movies")
+            Activities.Items.Add("Read a book by the fireplace ")
+            Activities.Items.Add(" Play board games with friends or family")
+
+            MsgBox("Hi, since it is rainy outside. Here are your daily activities: Stay indoors and watch movies, Read a book by the fireplace or Play board games with friends or family.")
+
+        Else
+            MsgBox("ghy ")
+
+        End If
+
+
+
+    End Sub
+End Class
